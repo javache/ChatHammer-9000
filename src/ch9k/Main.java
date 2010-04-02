46f463611b6c1a628d4195c1ddb72c0c5a1435b5
Pieter De Baets <pieter.debaets@gmail.com>
2010-04-02 14:53:08 +0200

    Initial commit, added testing framework

---
+++ src/ch9k/Main.java
@@ -0,0 +1,15 @@
+
+package ch9k;
+
+/**
+ * @author Pieter De Baets
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ test/ch9k/MainTest.java
@@ -0,0 +1,26 @@
+
+package ch9k;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Pieter De Baets
+ */
+public class MainTest {
+
+    public MainTest() {}
+
+	/**
+	 * Test of main method, of class Main.
+	 */
+	@Test
+	public void testMain() {
+		System.out.println("main");
+		String[] args = null;
+		Main.main(args);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+}
