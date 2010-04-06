e7325dee58f2279f97cb03067700b6d7d68eacdc
Toon Willems <willemstoon@gmail.com>
2010-04-06 11:45:42 +0200

    more stubs

--- src/ch9k/network/Connection.java
+++ src/ch9k/network/Connection.java
@@ -1,8 +1,39 @@
 package ch9k.network;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Queue;
+
+import ch9k.eventpool.NetworkEvent;
+
 /**
  * 
  * @author nudded
  */
 public class Connection {
+    
+    private static int DEFAULT_PORT = 1337;
+    
+    // the remote ip
+    private InetAddress target;
+
+    // a queue of events to be send
+    private Queue<NetworkEvent> sendQueue;
+
+    /**
+     * Constructor
+     * @param ip 
+     */
+    public Connection(InetAddress ip) {
+        target = ip;
+    }
+    
+    /**
+     * Send a NetworkEvent
+     * @param networkEvent
+     */
+    public void sendEvent(NetworkEvent networkEvent) {
+        
+    }
     
 }
