2431b54ab9945e1096bdf30786068c9fc97fa027
Toon Willems <willemstoon@gmail.com>
2010-04-06 11:30:36 +0200

    added imports, private fields

--- src/ch9k/network/ConnectionManager.java
+++ src/ch9k/network/ConnectionManager.java
@@ -1,10 +1,18 @@
 package ch9k.network;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.net.InetAddress;
+
+import ch9k.eventpool.NetworkEvent;
+
 /**
  * Handles all connections to remote hosts
  * @author nudded
  */
 public class ConnectionManager {
+
+    private Map<InetAddress,Connection> connectionMap;
     
     /**
      * Send a NetworkEvent
