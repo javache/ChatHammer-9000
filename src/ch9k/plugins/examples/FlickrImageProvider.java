31e41f1aa0f13924b3f47ed4197aa9f4cb397c60
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-04-06 10:55:14 +0200

    FlickImageProvider can now search Flickr.
    For now, it uses some logging to debug results. I'll try
    to write up some unit tests later if I can come up with
    some good test cases.

--- src/ch9k/plugins/examples/FlickrImageProvider.java
+++ src/ch9k/plugins/examples/FlickrImageProvider.java
@@ -1,6 +1,11 @@
 package ch9k.plugins.examples;
 
 import com.aetrion.flickr.Flickr;
+import com.aetrion.flickr.photos.PhotosInterface;
+import com.aetrion.flickr.photos.SearchParameters;
+import com.aetrion.flickr.photos.PhotoList;
+import com.aetrion.flickr.photos.Photo;
+import java.util.logging.Logger;
 
 /**
  * Class that gives us the ability to look up images on Flickr.
@@ -18,9 +23,67 @@ public class FlickrImageProvider {
     private final Flickr flickr;
 
     /**
+     * Logger, well, to log.
+     */
+    private final static Logger LOGGER =
+            Logger.getLogger("ch9k.plugins.examples.FlickrImageProvider");
+
+    /**
      * Constructor.
      */
     FlickrImageProvider() {
         flickr = new Flickr(API_KEY);
+    }
+
+    /**
+     * Search for some text on Flickr.
+     * @param text Text to search for.
+     * @return List of URL's.
+     */
+    public String[] searchFlickr(String text) {
+        return searchFlickr(text, 10);
+    }
+
+    /**
+     * Search for some text on Flickr.
+     * @param text Text to search for.
+     * @param maxResults Maximum results to return.
+     * @return List of URL's.
+     */
+    public String[] searchFlickr(String text, int maxResults) {
+        /* We first need to initialize our photos interface, and construct
+         * search parameters based on the text. */
+        PhotosInterface photosInterface = flickr.getPhotosInterface();
+        SearchParameters searchParameters = new SearchParameters();
+        searchParameters.setText(text);
+        LOGGER.info("Searching for: " + text);
+
+        /* Now search for the text. We limit ourselves to 10 photos. */
+        PhotoList photoList;
+        try {
+            photoList = photosInterface.search(searchParameters, maxResults, 0);
+        } catch (Exception exception) {
+            // TODO: Do something proper.
+            return null;
+        }
+
+        /* Put the results in a list of URL's, and log it as well. */
+        String[] urls = new String[photoList.size()];
+        for(int i = 0; i < urls.length; i++) {
+            Photo photo = (Photo) photoList.get(i);
+            urls[i] = photo.getMediumUrl();
+            LOGGER.info("Photo result: " + photo.getTitle() + " - " +
+                    photo.getMediumUrl());
+        }
+
+        return urls;
+    }
+
+    /**
+     * Main function for testing purposes. Searches for "lolcat" on Flickr.
+     */
+    public static void main(String[] args) {
+        FlickrImageProvider f = new FlickrImageProvider();
+        f.searchFlickr("lolcat");
     }
 }
