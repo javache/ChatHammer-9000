ff1da2ba0bfd870c7b5ca95de9ab061ffc9604b0
Toon Willems <willemstoon@gmail.com>
2010-04-06 14:45:59 +0200

    listen to incoming messages

--- src/ch9k/network/Connection.java
+++ src/ch9k/network/Connection.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Queue;
 import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 import java.io.IOException;
 
 import ch9k.eventpool.NetworkEvent;
@@ -22,7 +23,11 @@ public class Connection {
     // a queue of events to be send
     private Queue<NetworkEvent> sendQueue;
 
+    // the actual socket used to transfer objects
     private Socket socket;
+
+    // used by the listenThread to see if it should keep listening 
+    private boolean keepListening;
 
     /**
      * Constructor
@@ -30,10 +35,34 @@ public class Connection {
      */
     public Connection(InetAddress ip) throws IOException{
         socket = new Socket(ip,DEFAULT_PORT);
+        keepListening = true;
+        startListenThread();
     }
     
     public Connection(String host) throws IOException {
-        socket = new Socket(host,DEFAULT_PORT);        
+        socket = new Socket(host,DEFAULT_PORT);
+        keepListening = true;
+        startListenThread();
+    }
+    
+    /**
+     * close the socket
+     */
+    public void close() {
+        keepListening = false;
+        try {
+            socket.close();
+        } catch(IOException e) {
+            
+        }
+    }
+    
+    /**
+     * send a NetworkEvent
+     * @param ev The event to be send
+     */
+    public void sendEvent(NetworkEvent ev){
+        sendObject(ev);
     }
     
     /**
@@ -47,6 +76,27 @@ public class Connection {
         } catch(IOException e) {
             // TODO do something with that exception
         }
+    }
+    
+    /**
+     * start listening for incoming events and send them to the EventPool
+     */
+    private void startListenThread() {
+        new Thread(new Runnable(){
+            public void run() {
+                try {
+                    ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                    while(keepListening) {
+                        NetworkEvent ev = (NetworkEvent)in.readObject();
+                        EventPool.getInstance().raiseEvent(ev);
+                    }
+                } catch (IOException e) {
+                    
+                } catch (ClassNotFoundException e) {
+                    
+                }
+            }
+        }).start();
     }
     
 }
