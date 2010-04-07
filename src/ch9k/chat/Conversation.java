cf16a1987752832ce22b5452855541f336c9cfab
Jens Panneel <Jens.Panneel@UGent.be>
2010-04-07 18:06:36 +0200

    Coversation no longer holds plugins. PluginManager will take care of those.

--- src/ch9k/chat/Conversation.java
+++ src/ch9k/chat/Conversation.java
@@ -1,11 +1,5 @@
 package ch9k.chat;
 
-import ch9k.plugins.Plugin;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * Represents a conversation between two users.
  * @author Jens Panneel
@@ -13,7 +7,6 @@ import java.util.Set;
 public class Conversation {
     private Contact contact;
     private boolean initiated;
-    private Map<Class<? extends Plugin>, Plugin> activePlugins;
     private ConversationSubject subject;
 
     /**
@@ -22,17 +15,9 @@ public class Conversation {
      * @param initiatedByMe Is this conversation started by me?
      * @param activePlugins The list of the current active plugins on the initiators side.
      */
-    public Conversation(Contact contact, boolean initiatedByMe, List<Class<? extends Plugin>> activePlugins) {
+    public Conversation(Contact contact, boolean initiatedByMe) {
         this.contact = contact;
         this.initiated = initiatedByMe;
-        this.activePlugins = new HashMap<Class<? extends Plugin>, Plugin>();
-        for(Class<? extends Plugin> plugin : activePlugins) {
-            try {
-                addPlugin(plugin);
-            } catch (Exception ex) {
-                // TODO catch it here
-            }
-        }
     }
 
     /**
@@ -65,37 +50,6 @@ public class Conversation {
      */
     public void setSubject(ConversationSubject subject) {
         this.subject = subject;
-    }
-
-    /**
-     * Get a list of all current active plugins from this conversation.
-     * @return activePlugins
-     */
-    public Set<Plugin> getActivePlugins() {
-        return (Set<Plugin>) activePlugins.values();
-    }
-
-    /**
-     * Delete the instance of this plugin from this conversation
-     * @param plugin Class<? extends Plugin>
-     */
-    public void deletePlugin(Class<? extends Plugin> plugin) {
-        activePlugins.remove(plugin);
-    }
-
-    /**
-     * Add an instance of plugin to this conversation, throws exception when there is already such an instance
-     * @param plugin Class<? extends Plugin>
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * will throw plugin conflict exception
-     */
-    public void addPlugin(Class<? extends Plugin> plugin) throws InstantiationException, IllegalAccessException {
-        if(!activePlugins.keySet().contains(plugin)) {
-            activePlugins.put(plugin, plugin.newInstance());
-        } else {
-            // TODO throw plugin conflict exception
-        }
     }
 
     /**
