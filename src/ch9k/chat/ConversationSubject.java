03e50db1ef024ce06a84c8d44adc1014f8c2dfdf
Jens Panneel <Jens.Panneel@UGent.be>
2010-04-07 20:40:03 +0200

    ConversationSubject now has an constructor and now knows to which conversation it belongs to

--- src/ch9k/chat/ConversationSubject.java
+++ src/ch9k/chat/ConversationSubject.java
@@ -6,6 +6,16 @@ package ch9k.chat;
  */
 public class ConversationSubject {
     private String[] subjects;
+    private Conversation conversation;
+
+    public ConversationSubject(Conversation conversation, String[] subjects) {
+        this.conversation = conversation;
+        this.subjects = subjects;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
 
     public String[] getSubjects() {
         return subjects;
