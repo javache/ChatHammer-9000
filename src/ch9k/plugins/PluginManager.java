7322d7e2853cb0666636ef884ce5af43eb876306
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-04-07 16:15:07 +0200

    Added psuedocode to PluginManager.

--- src/ch9k/plugins/PluginManager.java
+++ src/ch9k/plugins/PluginManager.java
@@ -1,10 +1,29 @@
 package ch9k.plugins;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import ch9k.chat.Conversation;
+
 /**
  * A singleton to manage plugins.
  * @author Jasper Van der Jeugt
  */
 public class PluginManager {
+    /**
+     * We keep, for every conversation, a list of activated plugins by name.
+     */
+    private Map<Conversation, Set<String>> enabledPlugins;
+
+    /**
+     * In addition, we keep for every conversation the list of actual plugin
+     * instances.
+     */
+    private Map<Conversation, List<Plugin>> plugins;
+
     /**
      * Constructor.
      */
@@ -16,5 +35,83 @@ public class PluginManager {
      * @param name Name of the plugin to load.
      */
     public void loadPlugin(String name) {
+        // TODO: Do we need this?
+    }
+
+    /**
+     * Get a list of available plugins.
+     * @return A list of available plugins.
+     */
+    public String[] getAvailablePlugins() {
+        /* This list is stored in the configuration. We fetch it from there and
+         * return it. 
+         */
+        String[] availablePlugins = {
+            "ch9k.plugins.googleimage.GoogleImageProviderPlugin",
+            "ch9k.plugins.flickr.FlickrImageProviderPlugin"
+        };
+                                   
+        return availablePlugins;
+    }
+
+    /**
+     * Enable a plugin for a given conversation.
+     * @param conversation Conversation to enable the plugin for.
+     * @param name Name of the plugin to load.
+     */
+    public void enablePlugin(Conversation conversation, String name) {
+        /* Check that the plugin is not already enabled for the conversation. */
+        Set<String> conversationPlugins = enabledPlugins.get(conversation);
+        if(conversationPlugins != null && conversationPlugins.contains(name)) {
+            return;
+        }
+
+        /* Find the class of the new plugin and initialize it. */
+        Plugin plugin;
+        try {
+            Class pluginClass = Class.forName(name);
+            plugin = (Plugin) pluginClass.newInstance();
+        } catch (ClassNotFoundException exception) {
+            // TODO: Show relevant warning.
+            return;
+        } catch (InstantiationException exception) {
+            // TODO: Show relevant warning.
+            return;
+        } catch (IllegalAccessException exception) {
+            // TODO: Show relevant warning.
+            return;
+        }
+
+        /* Couple it with the conversation. */
+        // TODO: Couple with conversation.
+
+        /* Register plugin. */
+        if(conversationPlugins == null) {
+            conversationPlugins = new HashSet<String>();
+            List<Plugin> tmp = new ArrayList<Plugin>();
+            plugins.put(conversation, tmp);
+        }
+        conversationPlugins.add(name);
+        plugins.get(conversation).add(plugin); 
+    }
+
+    /**
+     * Disable a plugin for a given conversation.
+     * @param conversation Conversation to disable the plugin for.
+     * @param name Name of the plugin to disable.
+     */
+    public void disablePlugin(Conversation conversation, String name) {
+        Set<String> names = enabledPlugins.get(conversation);
+        List<Plugin> instances = plugins.get(conversation);
+
+        if(names != null && names.contains(name)) {
+            names.remove(name);
+            for(Plugin plugin: instances) {
+                if(plugin.getClass().getName().equals(name)) {
+                    // TODO: Disable plugin, so it doesn't listen anymore.
+                    instances.remove(plugin);
+                }
+            }
+        }
     }
 }
