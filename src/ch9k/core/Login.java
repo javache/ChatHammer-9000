e32e0457b8a999f3532e2a8cd72ded102d8cb9a3
Toon Willems <willemstoon@gmail.com>
2010-04-09 14:35:11 +0200

    added Login stub

---
+++ src/ch9k/core/Login.java
@@ -0,0 +1,25 @@
+package ch9k.core;
+
+import ch9k.configuration.Storage;
+
+/**
+ * The login model (not the gui)
+ * handles the loading of the account (and verifying it)
+ */
+public class Login {
+    
+    /**
+     * this method has to be called when a user wants to login
+     */
+    public void loadAccount(String username, String password) {
+        
+    }
+    
+    /**
+     * creates a new account
+     */
+    public void newAccount(String username,String password) {
+        
+    }
+    
+} 
