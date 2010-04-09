be1d34191b207609d25b0bf35b753dd77eb48037
Jens Panneel <Jens.Panneel@UGent.be>
2010-04-09 20:06:11 +0200

    made the dummy NewConversationEvent a compling dummy

--- src/ch9k/chat/events/NewConversationEvent.java
+++ src/ch9k/chat/events/NewConversationEvent.java
@@ -1,9 +1,6 @@
 package ch9k.chat.events;
 
-import java.net.InetAddress;
-
 import ch9k.chat.Contact;
-import ch9k.eventpool.NetworkEvent;
 
 /**
  * This event will be sent when we start a new Conversation
@@ -11,10 +8,10 @@ import ch9k.eventpool.NetworkEvent;
  * @author nudded
  */
 
-public class NewConversationEvent extends NetworkEvent {
+public class NewConversationEvent extends ConversationEvent {
+
  
-    public NewConversationEvent(InetAddress target) {
-        super(target);
+    public NewConversationEvent(Contact contact) {
+        super(contact);
     } 
-    
 }
