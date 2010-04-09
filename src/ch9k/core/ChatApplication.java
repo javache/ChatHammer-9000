b09e008cfd773ccf4c70bdacfa127da6ecb46900
Jens Panneel <Jens.Panneel@UGent.be>
2010-04-09 12:20:37 +0200

    added getInstance() to ChatApplication

--- src/ch9k/core/ChatApplication.java
+++ src/ch9k/core/ChatApplication.java
@@ -7,10 +7,20 @@ package ch9k.core;
  */
 public class ChatApplication {
 
+    public static ChatApplication getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    /* Helper-class for singleton, aka Bill Pugh's method */
+    private static class SingletonHolder {
+         private static final ChatApplication INSTANCE = new ChatApplication();
+    }
+
+
     private Account account;
 
     public Account getAccount() {
         return account;
     }
-
+    
 }
