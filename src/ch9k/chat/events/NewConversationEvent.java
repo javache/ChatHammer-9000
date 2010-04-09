18db713061cea86d86b5c69774e65ae90e10ab3b
Toon Willems <willemstoon@gmail.com>
2010-04-09 13:42:45 +0200

    NewConversationEvent is just a mere stub now

--- src/ch9k/chat/events/NewConversationEvent.java
+++ src/ch9k/chat/events/NewConversationEvent.java
@@ -1,8 +1,9 @@
 package ch9k.chat.events;
 
-import ch9k.chat.Contact;
+import java.net.InetAddress;
 
-import ch9k.eventpool.NetworkEvent
+import ch9k.chat.Contact;
+import ch9k.eventpool.NetworkEvent;
 
 /**
  * This event will be sent when we start a new Conversation
@@ -11,15 +12,9 @@ import ch9k.eventpool.NetworkEvent
  */
 
 public class NewConversationEvent extends NetworkEvent {
-    
-    private Contact contact;
-    
-    public NewConversationEvent(Contact contact) {
-        this.contact = contact;
-    }
-    
-    public Contact getSource() {
-        return contact;
-    }
+ 
+    public NewConversationEvent(InetAddress target) {
+        super(target);
+    } 
     
 }
