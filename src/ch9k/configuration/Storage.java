5c3ae5e122fdac10b496729f8a5aa5f7a11df326
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-04-12 14:58:26 +0200

    Made getApplicationDirectory() method static.

--- src/ch9k/configuration/Storage.java
+++ src/ch9k/configuration/Storage.java
@@ -46,7 +46,7 @@ public class Storage {
 
         //Parse the XML file
         SAXBuilder parser = new SAXBuilder();
-        File file = new File(getFilePath(), username.toLowerCase());
+        File file = new File(getApplicationDirectory(), username.toLowerCase());
         try {
             Element root = parser.build(file).getRootElement();
             for (Object obj : root.getChildren()) {
@@ -83,7 +83,8 @@ public class Storage {
         //Now store it somewhere on the HD
         try {
             //Open the right file
-            File file = new File(getFilePath(), username.toLowerCase());
+            File file =
+                    new File(getApplicationDirectory(), username.toLowerCase());
             //Open the outpustream and write the XML
             FileOutputStream outputstream = new FileOutputStream(file);
             XMLOutputter outputter = new XMLOutputter();
@@ -126,7 +127,7 @@ public class Storage {
      *
      * @return File representing the directory in which our app stores it's userfiles.
      */
-    public File getFilePath(){
+    public static File getApplicationDirectory(){
         String userHome = System.getProperty("user.home", ".");
         File workingDirectory;
         String sysName = System.getProperty("os.name").toLowerCase();
@@ -158,6 +159,7 @@ public class Storage {
      * @return Exists boolean
      */
     public boolean fileTest(String username){
-        return new File(getFilePath(), username.toLowerCase()).exists();
+        return new File(getApplicationDirectory(),
+                username.toLowerCase()).exists();
     }
 }
