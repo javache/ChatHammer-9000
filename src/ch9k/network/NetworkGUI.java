0bb290cc7cbccd44f0b142fcb92a8fadce4bb3a0
Toon Willems <willemstoon@gmail.com>
2010-04-13 15:06:50 +0200

    test to see if it works in real life

---
+++ src/ch9k/network/NetworkGUI.java
@@ -0,0 +1,12 @@
+package ch9k.network;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class NetworkGUI extends JPanel {
+    
+    public void paintComponent(Graphics g) {
+        
+    }
+    
+}
---
+++ test/ch9k/network/RealLifeTestReceiver.java
@@ -0,0 +1,15 @@
+package ch9k.network;
+
+
+import ch9k.eventpool.*;
+
+public class RealLifeTestReceiver {
+    
+    public static void main(String[] args) {
+        EventPool pool = EventPool.getAppPool();
+        while(true) {
+            
+        }
+    }
+    
+}
