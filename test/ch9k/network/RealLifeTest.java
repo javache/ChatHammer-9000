550d068aee0a7abd806bf124f7398bdcd3380d24
Pieter De Baets <pieter.debaets@gmail.com>
2010-04-14 17:28:40 +0200

    Enabled ant dependency tracking by default

--- test/ch9k/network/RealLifeTest.java
+++ test/ch9k/network/RealLifeTest.java
@@ -5,6 +5,7 @@ import ch9k.eventpool.EventListener;
 import ch9k.eventpool.EventFilter;
 import ch9k.eventpool.Event;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,24 +15,22 @@ public class RealLifeTest {
         
         public boolean received = false;
         
-        public void handleEvent(Event ev) {
+        public synchronized void handleEvent(Event ev) {
             TestNetworkEvent event = (TestNetworkEvent)ev;
             if(event.isExternal()) {
                 received = true;
-                synchronized(this) {
-                    notifyAll();
-                }
+                notifyAll();
             }
         }
-        
     }
     
     @Test
-    public void letTheBeastGo() throws Exception {
+    public void letTheBeastGo() throws UnknownHostException, InterruptedException {
         EventPool pool = EventPool.getAppPool();
         TestListener listener = new TestListener();
         pool.addListener(listener,new EventFilter(TestNetworkEvent.class));
         pool.raiseEvent(new TestNetworkEvent(InetAddress.getByName("zeus.ugent.be")));
+        
         synchronized(this) {
             wait(600);            
         }
