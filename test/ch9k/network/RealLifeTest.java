00b2f79cd3ef407688a827ea33148fe5e2f5ae0e
Toon Willems <willemstoon@gmail.com>
2010-04-14 14:37:53 +0200

    RealLifeTest is now automated

--- test/ch9k/network/RealLifeTest.java
+++ test/ch9k/network/RealLifeTest.java
@@ -6,10 +6,34 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RealLifeTest {
+<<<<<<< HEAD
+=======
+    
+    private class TestListener implements EventListener {
+        
+        public boolean received = false;
+        
+        public void handleEvent(Event ev) {
+            TestNetworkEvent event = (TestNetworkEvent)ev;
+            if(event.isExternal()) {
+                received = true;
+                synchronized(this) {
+                    notifyAll();
+                }
+            }
+        }
+        
+    }
+    
     @Test
     public void letTheBeastGo() throws Exception {
         EventPool pool = EventPool.getAppPool();
+        TestListener listener = new TestListener();
+        pool.addListener(listener,new EventFilter(TestNetworkEvent.class));
         pool.raiseEvent(new TestNetworkEvent(InetAddress.getByName("zeus.ugent.be")));
-        Thread.sleep(600);
+        synchronized(this) {
+            wait(600);            
+        }
+        assertTrue(listener.received);
     }
 }
