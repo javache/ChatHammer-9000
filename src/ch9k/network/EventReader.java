188b6a65e987d5a3f7b86322c06dc47209f0d43d
Toon Willems <willemstoon@gmail.com>
2010-04-16 10:20:04 +0200

    woops, forgot some funny comments

--- src/ch9k/network/EventReader.java
+++ src/ch9k/network/EventReader.java
@@ -10,14 +10,29 @@ import ch9k.eventpool.Event;
 
 public class EventReader implements Runnable {
     
+    /**
+     * Events will be read from this stream
+     */
     private final ObjectInputStream in;
-
+    
+    /**
+     * no poolparty without a pool!
+     */
     private final EventPool pool;
     
+    /**
+     * he's here to help, in case something bad happens
+     */
     private final ErrorHandler errorHandler;
     
+    /**
+     * processing is all he does, all day long
+     */
     private final EventProcessor processor;
     
+    /**
+     * the usual logger.
+     */
     private static final Logger logger = Logger.getLogger(EventReader.class);
     
     public EventReader(ObjectInputStream stream,EventPool pool,EventProcessor processor,ErrorHandler handler) {
@@ -27,6 +42,9 @@ public class EventReader implements Runnable {
         this.errorHandler = handler;
     }
     
+    /**
+     * Run forever, reading events from the inputstream
+     */
     public void run() {
         try {
             while(true) {
