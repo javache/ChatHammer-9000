3786b0a71392bd6ac60107672313fab3e31dc4f9
Toon Willems <willemstoon@gmail.com>
2010-04-21 10:27:08 +0200

    added EventWriterTest to NetworkSuite

--- test/ch9k/network/NetworkSuite.java
+++ test/ch9k/network/NetworkSuite.java
@@ -9,6 +9,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         ch9k.network.ConnectionTest.class,
-        ch9k.network.ConnectionManagerTest.class
+        ch9k.network.ConnectionManagerTest.class,
+        ch9k.network.EventWriterTest.class
 })
 public class NetworkSuite {}
