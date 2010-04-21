0a59a77736f6e65765755122c16e4be3c66b78ac
Toon Willems <willemstoon@gmail.com>
2010-04-21 10:20:20 +0200

    added eventwriter test, and it doesn't even need interwebs!1!

---
+++ test/ch9k/network/EventWriterTest.java
@@ -0,0 +1,62 @@
+package ch9k.network;
+
+import ch9k.eventpool.Event;
+import ch9k.eventpool.EventFilter;
+import ch9k.eventpool.EventListener;
+import ch9k.eventpool.EventPool;
+import ch9k.eventpool.NetworkEvent;
+import ch9k.network.events.PingEvent;
+import java.io.IOException;
+import java.io.File;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EventWriterTest {
+    
+    private class Handler implements ErrorHandler {
+        public void receivedEOF(){
+            
+        }
+        
+        public void writingFailed(){
+            fail("sending event failed");
+        }
+    }
+    
+    @Test
+    public void testSendEvent() throws Exception {
+        /* just a temp file, will be deleted */
+        File file = new File("temp");
+        file.createNewFile();
+        
+        /* hook up our input and outputstreams */
+        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+        /* we could add more logic to this handler, but this will do for now */
+        ErrorHandler handler = new Handler();
+        /* technically we don't even use this queue */
+        BlockingQueue<NetworkEvent> queue = new LinkedBlockingQueue<NetworkEvent>();
+        
+        /* send an event (synchronous) and read it back */
+        EventWriter writer = new EventWriter(stream,handler,queue);
+        writer.sendEvent(new PingEvent(InetAddress.getLocalHost()));
+        NetworkEvent ev = (NetworkEvent)in.readObject();
+        
+        /* we send a PingEvent, so this should not fail and be true */
+        assertTrue(ev instanceof PingEvent);
+        /* delete our file */
+        file.delete();
+    }
+    
+    
+}
