ad0e3af79265fa8b456be0bfef3966dfef99085c
Bruno Corijn <bruno.corijn@gmail.com>
2010-05-01 16:14:32 +0200

    Added some color stuff, and improved the fontSize and fontFamily actions

--- src/ch9k/chat/gui/FontPanel.java
+++ src/ch9k/chat/gui/FontPanel.java
@@ -4,10 +4,12 @@
  */
 package ch9k.chat.gui;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Action;
+import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
@@ -18,6 +20,7 @@ import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import javax.swing.text.StyledEditorKit;
 
 /**
@@ -25,7 +28,6 @@ import javax.swing.text.StyledEditorKit;
  * @author Bruno
  */
 public class FontPanel extends JPanel implements ActionListener, CaretListener {
-
     /**
      * The textpanel that we want to style
      */
@@ -35,27 +37,37 @@ public class FontPanel extends JPanel implements ActionListener, CaretListener {
      * The actions used to style the text
      */
     private Action boldAction = new StyledEditorKit.BoldAction();
+
     private Action underlineAction = new StyledEditorKit.UnderlineAction();
+
     private Action italicAction = new StyledEditorKit.ItalicAction();
 
     /**
      * The toggle buttons that will use the actions above
      */
     private JToggleButton bold;
+
     private JToggleButton italic;
+
     private JToggleButton underline;
 
     /**
      * Arrays with the available options for the comboboxes
      */
     private String[] fontTypes = { "Serif", "SansSerif", "Monospaced" };
+
     private String[] fontSizes = { "10", "12", "14", "16", "18", "20" };
+
+    private Color[] colors = { Color.BLACK, Color.GRAY, Color.BLUE, Color.RED, Color.GREEN, Color.ORANGE, Color.CYAN, Color.MAGENTA };
 
     /**
      * Comboxes in which the user wil be able to select above options
      */
     private JComboBox fontType;
+
     private JComboBox fontSize;
+
+    private JComboBox colorBox;
 
     public FontPanel(JTextPane textpane) {
         editor = textpane;
@@ -93,28 +105,34 @@ public class FontPanel extends JPanel implements ActionListener, CaretListener {
         fontSize.setSelectedItem("14");
         add(fontSize);
 
+        colorBox = new JComboBox(colors);
+        colorBox.addActionListener(this);
+        ColorRenderer renderer = new ColorRenderer();
+        renderer.setSize(30, 20);
+        colorBox.setRenderer(renderer);
+        add(colorBox);
+
     }
 
-
     public void actionPerformed(ActionEvent e) {
+        /*All the text in the editor should change by this action, so get the attributes */
+        MutableAttributeSet attrs = editor.getInputAttributes();
         /* Check which of the comboboxes fired this event */
-        if (e.getSource() == fontType) {
-            int index = (((JComboBox) e.getSource()).getSelectedIndex());
-            /*All the text in the editor should change by this action, so select all of it */
-            int pos = editor.getCaretPosition();
-            editor.selectAll();
-            new StyledEditorKit.FontFamilyAction("set-font-family", fontTypes[index]).actionPerformed(e);
-            /*Set the caret back to it's original position*/
-            editor.setCaretPosition(pos);
-        } else if (e.getSource() == fontSize) {
-            int index = (((JComboBox) e.getSource()).getSelectedIndex());
-            /*All the text in the editor should change by this action, so select all of it */
-            int pos = editor.getCaretPosition();
-            editor.selectAll();
-            new StyledEditorKit.FontSizeAction("set-font-size", Integer.parseInt(fontSizes[index])).actionPerformed(e);
-            /*Set the caret back to it's original position*/
-            editor.setCaretPosition(pos);
+        if(e.getSource() == fontType) {
+            int index = fontType.getSelectedIndex();
+            StyleConstants.setFontFamily(attrs, fontTypes[index]);
+        } else if(e.getSource() == fontSize) {
+            int index = fontSize.getSelectedIndex();
+            StyleConstants.setFontSize(attrs, Integer.parseInt(fontSizes[index]));
+
+        } else if(e.getSource() == colorBox) {
+            int index = colorBox.getSelectedIndex();
+            StyleConstants.setForeground(attrs, colors[index]);
         }
+        /*Change the attributes to the ones we changed above*/
+        StyledDocument doc = editor.getStyledDocument();
+        doc.setCharacterAttributes(0, doc.getLength() + 1, attrs, false);
+
     }
 
     @Override
