8898a7d1e4a247eedaa806b2d4874f16be6e25c2
Toon Willems <willemstoon@gmail.com>
2010-05-04 11:57:25 +0200

    fixed a bug in ContactOnlineListener

--- src/ch9k/chat/ContactList.java
+++ src/ch9k/chat/ContactList.java
@@ -166,7 +166,7 @@ public class ContactList extends AbstractListModel
             /* all this has to be true, otherwise we just have to ignore */
             if(onlineEvent.isExternal() && contact != null &&
                     !contact.isIgnored() && !contact.isBlocked()) {
-                if(onlineEvent.requiresReponse()) {
+                if(onlineEvent.requiresReponse() || !contact.isOnline()) {
                     EventPool.getAppPool().raiseNetworkEvent(new ContactOnlineEvent(contact));
                     
                     /* keep in mind here that this will set the state to online
