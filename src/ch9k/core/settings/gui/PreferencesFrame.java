dc434861993bdda13fe51835f956dcd29eee5916
Toon Willems <willemstoon@gmail.com>
2010-05-07 10:52:43 +0200

    Added a better gui to the PreferencesFrame.

--- src/ch9k/core/settings/gui/PreferencesFrame.java
+++ src/ch9k/core/settings/gui/PreferencesFrame.java
@@ -5,8 +5,16 @@ import ch9k.eventpool.Event;
 import ch9k.eventpool.EventFilter;
 import ch9k.eventpool.EventListener;
 import ch9k.eventpool.EventPool;
+import java.awt.CardLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  *
@@ -14,22 +22,58 @@ import javax.swing.JTabbedPane;
  */
 public class PreferencesFrame extends JFrame implements EventListener {
 
-    private JTabbedPane tabbedPane;
+    private CardLayout layout;
+    private DefaultListModel listModel;
+    private JPanel prefPane;
 
     public PreferencesFrame() {
         super("Preferences");
 
         EventPool.getAppPool().addListener(this, new EventFilter(PreferencePaneEvent.class));
 
-        tabbedPane = new JTabbedPane();
-        setContentPane(tabbedPane);
+        JPanel mainPanel = new JPanel();
+
+        layout = new CardLayout();
+        prefPane = new JPanel(layout);
+
+        listModel = new DefaultListModel();
+        final JList list = new JList(listModel);
+
+        JScrollPane listPane = new JScrollPane(list);
+
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                layout.show(prefPane, (String) listModel.get(list.getSelectedIndex()));
+            }
+        });
+
+        GroupLayout groupLayout = new GroupLayout(mainPanel);
+        mainPanel.setLayout(groupLayout);
+
+        groupLayout.setAutoCreateContainerGaps(true);
+        groupLayout.setAutoCreateGaps(true);
+
+        groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
+                .addComponent(listPane)
+                .addComponent(prefPane));
+
+        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+                .addGroup(groupLayout.createParallelGroup()
+                    .addComponent(listPane)
+                    .addComponent(prefPane)));
+
+        setContentPane(mainPanel);
         pack();
     }
 
     @Override
     public void handleEvent(Event ev) {
         PreferencePaneEvent event = (PreferencePaneEvent)ev;
-        tabbedPane.add(event.getTitle(), event.getPanel());
+
+        listModel.addElement(event.getTitle());
+        prefPane.add(event.getPanel(), event.getTitle());
+
         pack();
         repaint();
     }
